8e0c96f2b57d1c5a9da8d0e01be91450b8c7a9aa
Mark Laverick <Mark.Laverick@uk.delarue.com>
2019-11-29 13:34:51 +0000

    terraform updated variables

--- variables.tf
+++ variables.tf
@@ -10,3 +10,24 @@ variable "azurerm_subscription_id" {}
 variable "azurerm_client_id" {}
 variable "azurerm_client_secret" {}
 variable "azurerm_tenant_id" {}
+
+# Test variables
+/*
+variable network_name {
+  default = "networkname"
+  type = "string"
+  description = "Network name for the platform"
+}
+variable ip_cidr_range {
+  default = "10.0.0.0/16"
+  type = "string"
+  description = "IP CIDR Range for the platform"
+}
+variable subnet_names {
+  type = "map"
+  default = {
+    subnet1 = "subnetone"
+    subnet2 = "subnettwo"
+    subnet3 = "subnetthree"
+  }
+}*/
